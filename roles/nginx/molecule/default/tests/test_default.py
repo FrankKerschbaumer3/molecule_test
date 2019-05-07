4fdfa36dfb523d349bccbcca162a0167475769a4
Frankkerschbaumer3 <Frankkerschbaumer3@gmail.com>
2019-05-07 17:04:40 -0400

    Adjusted test.py and adjusted nginx tasks/main.yml

--- roles/nginx/molecule/default/tests/test_default.py
+++ roles/nginx/molecule/default/tests/test_default.py
@@ -11,11 +11,21 @@ def nginx_config_exists(host):
     assert nginx_config_exists
 
 
+def nginx_index_exists(host):
+    host = host.file("/etc/nginx/nginx.conf").exists
+    assert nginx_index_exists
+
+
 def test_nginx_is_installed(host):
     host = host.package("nginx")
     assert test_nginx_is_installed
 
 
+def test_curl_is_installed(host):
+    host = host.package("curl")
+    assert test_nginx_is_installed
+
+
 def nginx_is_hosting(host):
-    host = host.socket("tcp://0.0.0.0:80").is_listening
+    host = host.socket("tcp://1.0.0.0:80").is_listening
     assert nginx_is_hosting
