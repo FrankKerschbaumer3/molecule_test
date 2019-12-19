f95481d967b4a0473cd3ba810a809810c9216c98
Frank Kerschbaumer <frank.kerschbaumer@slalom.com>
2019-12-19 09:22:07 -0500

    Pushed up changes necessary for full run.

--- roles/nginx/molecule/default/tests/test_default.py
+++ roles/nginx/molecule/default/tests/test_default.py
@@ -1,21 +1,21 @@
 import os
 import testinfra.utils.ansible_runner
 
-
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def nginx_config_exists(host):
-    host = host.file("/var/www/html/index.html").exists
-    assert nginx_config_exists
-
-
 def test_nginx_is_installed(host):
-    host = host.package("nginx")
-    assert test_nginx_is_installed
+    ngx = host.package("nginx")
+    assert ngx.is_installed
 
 
-def nginx_is_hosting(host):
-    host = host.socket("tcp://0.0.0.0:80").is_listening
-    assert nginx_is_hosting
+def test_nginx_running_and_enabled(host):
+    ngx = host.service("nginx")
+    assert ngx.is_running
+    assert ngx.is_enabled
+
+
+def test_nginx_listening_http(host):
+    ngx = host.socket('tcp://0.0.0.0:80')
+    assert ngx.is_listening
